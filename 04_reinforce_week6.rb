4db70d5c5102444fc388d98bc3431c1584ac425c
Daniel Ang <dan.justin.ang@gmail.com>
2018-10-04 16:36:30 -0400

    completed reinforcement

---
+++ 04_reinforce_week6.rb
@@ -0,0 +1,12 @@
+
+Array(1..20).each do |number|
+  if number == 1
+    puts "#{number}st"
+  elsif number == 2
+    puts "#{number}nd"
+  elsif number == 3
+    puts "#{number}rd"
+  else
+    puts "#{number}th"
+  end
+end
